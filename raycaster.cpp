4c0af07b86b0b0ce8314788bffd7bd696ec447be
Jeffrey Abreu <jeffreyabreu66@gmail.com>
2026-04-02 19:32:14 -0400

    working on player

--- raycaster.cpp
+++ raycaster.cpp
@@ -23,7 +23,7 @@ int main()
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "Raycaster", NULL, NULL);
     if(window == NULL) {
@@ -45,7 +45,7 @@ int main()
     glViewport(0, 0, 800, 600);
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    px = 300, py = 400;
+    px = 0.0f, py = 0.0f;
     while(!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
